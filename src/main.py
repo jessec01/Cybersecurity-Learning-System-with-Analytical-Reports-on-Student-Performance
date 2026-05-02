93db96657a36c27f8570b47e5de4ceb136903d23
jessec01 <jegro@gmail.com>
2026-05-01 23:33:56 -0400

    feat:Creation of the software's main startup file

--- src/main.py
+++ src/main.py
@@ -3,5 +3,6 @@ from infrastructure.server.server import create_application
 
 if __name__ == "__main__":
     app = create_application()
+    #arranque del programa
     uvicorn.run(app, host="localhost")
     
