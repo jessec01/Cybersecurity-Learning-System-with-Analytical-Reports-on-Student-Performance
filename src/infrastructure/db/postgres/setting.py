06f8273e018a67d9824a7a4b6ce962cb1d73934e
jessec01 <jegro@gmail.com>
2026-05-14 17:33:09 -0400

    feat:the Postgres credentials were configured

---
+++ src/infrastructure/db/postgres/setting.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class SettingsPostgres(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    #Construimos la url dinamica
+    @property
+    def get_postgres_url(self):
+        return f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
+
+settings=SettingsPostgres()
