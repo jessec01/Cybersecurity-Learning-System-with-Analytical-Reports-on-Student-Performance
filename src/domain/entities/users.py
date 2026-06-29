e882eb7f5a901a12fa3b5684020d93aa385aa8e1
jessec01 <jegro@gmail.com>
2026-06-28 22:24:29 -0400

    feat:The users entity was created

--- src/domain/entities/users.py
+++ src/domain/entities/users.py
@@ -1,13 +1,16 @@
+# pyrefly: ignore [missing-import]
 from pydantic import BaseModel
-from abc import ABC, abstractmethod
 class Users(BaseModel):
+    id_user: int | None = None
     username:str
     password:str
     is_email_verified:bool
     is_token_verified:bool
     is_token_reset:bool
     is_token_expired:bool
-    is_active:bool   
+    is_active:bool
+    roles: list[str] = []
+    permissions: list[str] = []
 
     def is_user_email_verified(self)->bool:
         """valida si el usuario esta verificado por correo"""
@@ -21,18 +24,10 @@ class Users(BaseModel):
     def is_user_token_expired(self)->bool:
         """valida si el token del usuario ha expirado"""
         return self.is_token_expired
-    def is_user_verified(self)->bool:
+    def is_user_verified(self):
         """llama los otro metodos que definire para validar si el usuario esta validado""" 
         if self.is_email_verified and self.is_token_verified:
             self.is_active=True
         else:
             self.is_active=False    
-    @property
-    def is_user_active(self)->bool:
-        """valida si el usuario esta activo"""
-        return self.is_active
-    @property
-    def username(self)->str:
-        """retorna el nombre de usuario"""
-        return self.username
     #"""logica de usuario definido aqui segun la demanda del sistema"""
